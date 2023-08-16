a4287e604625a96b8fde84baa5855e377ddccd2f
Bruno <brunogonzalez@users.noreply.github.com>
2023-08-16 16:17:49 +0200

    Adapting columns names for clarity and reporting

--- getDatabaseFilesDetails.sql
+++ getDatabaseFilesDetails.sql
@@ -1,19 +1,19 @@
-SELECT FileName = df.name
-	,df.physical_name
-	,current_file_size_MB = df.size*1.0/128
-	,max_size = 
+SELECT [File Name] = df.name
+	,[Physical Name]=df.physical_name
+	,[Current File Size (MB)] = FORMAT(df.size*1.0/128, '0.######')
+	,[Max File Size]= 
 	CASE df.max_size
 		WHEN 0 THEN 'Autogrowth is off.'
 		WHEN -1 THEN 'Autogrowth is on.'
 		ELSE 'Log file grows to a maximum size of 2 TB.'
 	END
-	,growth_value =
+	,[File Growth Value]=
      CASE
-       WHEN df.growth = 0 THEN df.growth
-       WHEN df.growth > 0 AND df.is_percent_growth = 0 THEN df.growth*1.0/128.0
-       WHEN df.growth > 0 AND df.is_percent_growth = 1 THEN df.growth
+       WHEN df.growth = 0 THEN FORMAT(df.growth, '0.######')
+       WHEN df.growth > 0 AND df.is_percent_growth = 0 THEN FORMAT(df.growth*1.0/128.0, '0.######')
+       WHEN df.growth > 0 AND df.is_percent_growth = 1 THEN FORMAT(df.growth, '0.######')
      END
-	 ,growth_increment_unit =
+	 ,[Growth Increment Unit]=
      CASE
        WHEN df.growth = 0 THEN 'Size is fixed.'
        WHEN df.growth > 0 AND df.is_percent_growth = 0  THEN 'Growth value is MB.'
